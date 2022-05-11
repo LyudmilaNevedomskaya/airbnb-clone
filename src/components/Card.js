0244719d9317344c0b4625d830ea43c49725e01c
Lyudmila <milo4kamoya@gmail.com>
2022-05-10 22:44:41 -0700

    Cards props changed

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,9 +4,9 @@ import React from "react";
 
 export default function Card(props) {
   let badgeText;
-  if (props.openSpots === 0) {
+  if (props.item.openSpots === 0) {
     badgeText = "SOLD OUT";
-  } else if (props.country === "Online") {
+  } else if (props.item.location === "Online") {
     badgeText = "ONLINE";
   } else {
     badgeText = ''
@@ -14,17 +14,17 @@ export default function Card(props) {
   return (
     <div className="card">
       {badgeText !== '' && <div className="card--status">{badgeText}</div>}
-      <img className="card--picture" src={require(`../images/${props.img}`)} alt=""/>
+      <img className="card--picture" src={require(`../images/${props.item.coverImg}`)} alt=""/>
       <div className="card--rating">
         <img src={require('../images/Star.png')} alt="" width="14px" height="14px"/>
-        <p>{props.rating}</p>
-        <p className="card--count_rating">({props.reviewCount})</p>
+        <p>{props.item.stats.rating}</p>
+        <p className="card--count_rating">({props.item.stats.reviewCount})</p>
         <ul>
-          <li>{props.country}</li>
+          <li>{props.item.location}</li>
         </ul>
       </div>
-      <p className="card--description">{props.title}</p>
-      <p className="card--price"><span>From ${props.price}</span> / person</p>
+      <p className="card--description">{props.item.title}</p>
+      <p className="card--price"><span>From ${props.item.price}</span> / person</p>
     </div>
   )
 }
