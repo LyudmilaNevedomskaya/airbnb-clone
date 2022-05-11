574563dc3fd4d95ac60c8cec58aa036ef5b7fe3c
Lyudmila <milo4kamoya@gmail.com>
2022-05-10 19:49:40 -0700

    Cards data added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
+import data from './data'
 //import image from './img/katie.png'
 
 function App() {
+  const card = data.map(item => {
+    return <Card
+      img={item.coverImg}
+      rating={item.stats.rating}
+      reviewCount={item.stats.reviewCount}
+      country={item.location}
+      title={item.title}
+      price={item.price}
+    />
+  })
   return (
     <div className="App">
       <Navbar />
       <Header />
-      <Card 
-        img='katie.png'
-        rating='5.0'
-        reviewCount={6}
-        country='USA'
-        title='Life Lessons with Katie Zaferes'
-        price={136}
-      />
+      <div className='main-content'>
+        {card}
+      </div>
     </div>
   );
 }
